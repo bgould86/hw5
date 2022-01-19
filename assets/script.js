3be97b2b666e2c1c2dac1dbfa0a041c6b5cc5b3b
bgould86 <bgould86@gmail.com>
2022-01-19 17:27:18 -0600

    set up and almost ready to submit

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+// global variables
+
+// functions
+// save tasks to local storage
+function saveTask() {
+  console.log($(this).parent());
+
+  const time = $(this).parent().attr("id");
+  const task = $(this).siblings("textarea").val();
+
+  localStorage.setItem(time, task);
+}
+
+function timeColorChange() {
+  const now = moment().hours();
+  $(".time-color").each(function () {
+    const row = $(this);
+    const rowHour = row.data("hour");
+    if (rowHour < now) {
+      $(this).addClass("past");
+    } else if (rowHour === now) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+}
+
+// event listeners
+// show current date
+$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+
+// retrieve tasks from local storage
+function localStoreHour() {
+  let hoursForScheduling = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  for (i = 0; i < hoursForScheduling.length; i++) {
+    $(`${"#hour" + hoursForScheduling[i]} .js-task`).val(localStorage.getItem(`${"hour" + hoursForScheduling[i]}`));
+  }
+}
+
+// user clicks save button
+$(".js-saveBtn").on("click", saveTask);
+timeColorChange();
+localStoreHour();
